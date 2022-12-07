83af7e62b30aa4b79a95d6ebcd2864405008aaf6
fabricemlili <fmlili@hotmail.fr>
2022-12-07 15:15:37 +0100

    logging on python and browser

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
 
@@ -20,5 +21,31 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
         type="button">
     Connect you bitches
 </button>
+<a href="logger" target="_self">Logger</a>
  """
+ print("watch out!")
  return prefix_google + "Hello World"
+
+
+
+
+@app.route('/logger', methods=["GET"])
+
+def logger():
+ prefix_google = """
+ <!-- Google tag (gstag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-250903244-1');
+</script>
+<script>
+ console.log('Watch out!')
+</script>
+
+ """
+ print("watch out!")
+ return prefix_google
