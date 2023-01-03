cc12f8470db8061ce02608f933a5b1ac36751a89
fabricemlili <fmlili@hotmail.fr>
2023-01-03 16:04:06 +0100

    TP2 et TP3

---
+++ TP3/main.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='template')
+app.config["DEBUG"] = True
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-250903244-1');
+</script>
+<a href="/trend">Google Trend</a><br>
+<a href="/counter">Counter</a><br>
+<a href="/time-execution">Log Time Execution</a><br>
+ """
+    return prefix_google
+
+###########################         Google trend        #############################################""
+
+from pytrends.request import TrendReq
+import pandas as pd
+#from datetime import datetime
+
+@app.route('/trend', methods=["GET"])
+def trend():
+
+    pytrends = TrendReq(hl='en-US', tz=360)
+
+    kw_list=['Yahoo','Google']
+    pytrends.build_payload(kw_list, timeframe='2022-01-09 2023-01-01', geo='US')
+    df = pytrends.interest_over_time()
+    yahoo_data = df['Yahoo'].values.tolist()
+    google_data = df['Google'].values.tolist()
+    dates = df.index.values.tolist()
+
+    params = {
+        "type": 'line',
+            "data": {
+                "labels": dates,
+                "datasets": [{
+                    "label": 'Yahoo',
+                    "data": yahoo_data,
+                    "borderColor": '#3e95cd',
+                    "fill": 'false',
+                },
+                {
+                    "label": 'Google',
+                    "data": google_data,
+                    "borderColor": '#ffce56',
+                    "fill": 'false',
+                }
+                ]
+            },
+            "options": {
+                "title": {
+                    "text": 'My Line Chart'
+                },
+                "scales": {
+                    "yAxes": [{
+                        "ticks": {
+                            "beginAtZero": 'true'
+                        }
+                    }]
+                }
+            }
+    }
+    
+    
+    
+    prefix_google = """
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+    <canvas id="myChart" width="50" height="50"></canvas>""" + f"""
+    <script>
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {params});
+    </script>
+
+    """
+
+    return prefix_google
+
+
+###########################        Timer Log        #############################################""
+
+import time
+from collections import Counter
+
+def log_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"Execution time: {end - start:.6f} seconds")
+        return result
+    return wrapper
+
+@app.route('/counter')
+@log_execution_time
+def counter():
+    # Download Shakespear artwork
+    with open('shakespeare.txt', 'r') as f:
+        text = f.read()
+    word_count_dict = count_words_dict(text)
+    word_count_counter = count_words_counter(text)
+    print(word_count_dict)
+    print(word_count_counter)
+    return f"{word_count_dict}<br><br><br>     {word_count_counter}"
+
+def count_words_dict(text):
+    word_count = {}
+    for word in text.split():
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+def count_words_counter(text):
+    return Counter(text.split())
+
+
+
+if __name__ == '__main__':
+    app.run()
--- first_micro/main.py
+++ first_micro/main.py
@@ -1,13 +1,18 @@
 from flask import Flask
-import logging
+import requests
+from lxml import html
+#Load Libraries
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+import httplib2
+
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET"])
-
-
 def hello_world():
- prefix_google = """
+    prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
 src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
@@ -17,35 +22,110 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
  gtag('js', new Date());
  gtag('config', 'UA-250903244-1');
 </script>
-<button class="favorite styled"
-        type="button">
-    Connect you bitches
-</button>
-<a href="logger" target="_self">Logger</a>
+<a href="/logger">Logger</a>
+<a href="/visitors"> Number of visitors</a>
+<a href="/trend"> Trend</a>
  """
- print("watch out!")
- return prefix_google + "Hello World"
-
-
+    print("watch out!")
+    return prefix_google + "Hello World"
 
 
 @app.route('/logger', methods=["GET"])
-
 def logger():
- prefix_google = """
+    prefix_google = """
  <!-- Google tag (gstag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
-<script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'UA-250903244-1');
-</script>
 <script>
  console.log('Watch out!')
 </script>
+<input id="textbox" type="text", size="40">
+<button id="enter">enter</button>
+
+<script>
+const element = document.getElementById("enter")
+
+element.addEventListener("click", myFunction)
+
+function myFunction(){
+    console.log(document.getElementById("textbox").value)
+}
+</script>
 
  """
- print("watch out!")
- return prefix_google
+ 
+    return prefix_google
+
+#####################################       TP2         ################################################""
+
+
+from flask import Flask, render_template
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'data-sources-373514-95e8e4622a9d.json'
+VIEW_ID = '281188353' #You can find this in Google Analytics > Admin > Property > View > View Settings (VIEW ID)
+
+
+def initialize_analyticsreporting():
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILE_LOCATION, SCOPES)
+  analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+  return analytics
+
+
+def get_report(analytics):
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:pageviews'}],
+          'dimensions': []
+        }]
+      }
+  ).execute()
+
+
+def get_visitors(response):
+  visitors = 0 # in case there are no analytics available yet
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+    for row in report.get('data', {}).get('rows', []):
+      dateRangeValues = row.get('metrics', [])
+
+      for i, values in enumerate(dateRangeValues):
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          visitors = value
+
+  return str(visitors)
+
+@app.route('/visitors', methods=["GET"])
+def visitors():
+  analytics = initialize_analyticsreporting()
+  response = get_report(analytics)
+  visitors = get_visitors(response)
+
+  prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-250903244-1');
+    </script>"""
+
+  return prefix_google + "The number of visitors this last month is " + str(visitors)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
